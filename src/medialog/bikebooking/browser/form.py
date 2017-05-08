e1cfbd5c9058d3b04fc67a2b8cb4144acc4e3379
Espen Moe-Nilssen <espen@medialog.no>
2017-05-08 13:09:18 +0200

    Added mime text (html) in mail body

--- src/medialog/bikebooking/browser/form.py
+++ src/medialog/bikebooking/browser/form.py
@@ -10,7 +10,9 @@ from Products.statusmessages.interfaces import IStatusMessage
 
 from datetime import *
 #from isoweek import Week
-
+from email.mime.text import MIMEText
+from email import message_from_string
+import hashlib
 
 
 
@@ -53,14 +55,33 @@ class BookingForm(form.SchemaForm):
             return
         if data['email'].endswith('medialog.no') or data['email'].endswith('asvg.no'):
             email = data['email']
+            checksum = hashlib.sha224(data['email']).hexdigest()
             name = data['name']
-            id = self.context.id
-            mailbody = 'Hei, ' + name + '\n' +  u'Vennligst klikk lenken og bekreft reserveringen' + '\n' +  self.context.absolute_url() + '/@@confirm-form?email=' + email + "&name=" + name.encode('utf8') \
-             + u'\n\n\n For senere fjerning av reserveringen, klikk her \n' +  self.context.absolute_url() + '/@@confirm-form?email=' + email + "&name=" + name.encode('utf8') + '&bestille=0'
+            #id = self.context.id
+            #mailbody = 'Hei, ' + name + '\n' +  u'Vennligst klikk lenken og bekreft reserveringen' + '\n' +  self.context.absolute_url() + '/@@confirm-form?email=' + email + "&name=" + name.encode('utf8') \
+            # + u'\n\n\n For senere fjerning av reserveringen, klikk her \n' +  self.context.absolute_url() + '/@@confirm-form?email=' + email + "&name=" + name.encode('utf8') + '&bestille=0'
+            
+            
+            html = """\
+                <html>
+                  <head></head>
+                  <body>
+                    <p>Hei, %{name}s<br>
+                       <a href="%{url}s/@@confirm-form?email=%{email}s&name=%{name}s&checksum=%{checksum}s"> Vennligst klikk p&aring; lenken og bekreft reserveringen"</a><br><br><br>
+                       <a href="%{url}s/@@confirm-form?email=%{email}s&name=%{name}s&bestille=0&checksum=%{checksum}s"> Hvis du senere trenger &aring; 'avbestille', klikk her</a>
+                    </p>
+                  </body>
+                </html>
+                """ % { 'name': name.encode('utf8'),
+                         'email': email,
+                         'url':  self.context.absolute_url(),
+                         'checksum': checksum,
+                    }
+            mailbody = MIMEText(html, 'html')
             api.portal.send_email(
                 recipient=data['email'],
                 subject="Sykkelreservasjon",
-                body=mailbody.encode('utf8'),
+                body= mailbody,
                 )
             IStatusMessage(self.request).addStatusMessage(
                 u"En epost blir straks sendt deg. \n Bekreft reservasjonen snarest mulig", "info"
@@ -75,8 +96,8 @@ class BookingForm(form.SchemaForm):
     def handleCancel(self, action):
         """User cancelled. Redirect back to the page.
         """
-        
-        
+    
+   
 class ConfirmForm(BrowserView):
     """
     Email confirm page
@@ -92,48 +113,49 @@ class ConfirmForm(BrowserView):
                 return this
         return False
     
-    def __call__(self, email="", name="", bestille=""):
+    def __call__(self, email="", name="", bestille="", checksum=0):
         context = self.context
         #bestille = self.request.bestille
         email  = self.request.email
         name   = self.request.name.encode('utf8') 
-        if email.endswith('medialog.no') or email.endswith('asvg.no'):
-            try:
-                if bestille == '0':
-                    self.context.person_pair.remove(self.in_dictlist('email', email))
-                    api.portal.send_email(
-                        recipient=data['email'],
-                        subject="Sykkelreservasjon",
-                        body=u'Din sykkelreservasjon har blitt fjernet',
-                    )
-                    IStatusMessage(self.request).addStatusMessage(
-                    u"Din reservering er fjernet",
-                    "Info"
-                    )
+        if self.request.checksum == hashlib.sha224(data['email']).hexdigest():
+            if email.endswith('medialog.no') or email.endswith('asvg.no'):
+                try:
+                    if bestille == '0':
+                        self.context.person_pair.remove(self.in_dictlist('email', email))
+                        api.portal.send_email(
+                            recipient=data['email'],
+                            subject="Sykkelreservasjon",
+                            body=u'Din sykkelreservasjon har blitt fjernet',
+                        )
+                        IStatusMessage(self.request).addStatusMessage(
+                        u"Din reservering er fjernet",
+                        "Info"
+                        )
+                        contextURL = api.portal.get().absolute_url()
+                        self.request.response.redirect(contextURL)
+                    elif len(context.person_pair) >= context.bikes:
+                        IStatusMessage(self.request).addStatusMessage(
+                        u"Alle sykler er reservert",
+                        "Info"
+                        )
+                        contextURL = api.portal.get().absolute_url()
+                        self.request.response.redirect(contextURL)
+                    else:
+                        if not self.in_dictlist('email', email):
+                            self.context.person_pair.append({'name': name, 'email': email})
+                    
+                        #   with api.env.adopt_roles(['Manager']):
+                        #   api.content.transition(context, transition='reserver')
+                    
+                        IStatusMessage(self.request).addStatusMessage(
+                            u"Din reservasjon er bekreftet",
+                            "info"
+                        )
                     contextURL = api.portal.get().absolute_url()
                     self.request.response.redirect(contextURL)
-                elif len(context.person_pair) >= context.bikes:
-                    IStatusMessage(self.request).addStatusMessage(
-                    u"Alle sykler er reservert",
-                    "Info"
-                    )
-                    contextURL = api.portal.get().absolute_url()
-                    self.request.response.redirect(contextURL)
-                else:
-                    if not self.in_dictlist('email', email):
-                        self.context.person_pair.append({'name': name, 'email': email})
-                    
-                    #   with api.env.adopt_roles(['Manager']):
-                    #   api.content.transition(context, transition='reserver')
-                    
-                    IStatusMessage(self.request).addStatusMessage(
-                        u"Din reservasjon er bekreftet",
-                        "info"
-                    )
-                contextURL = api.portal.get().absolute_url()
-                self.request.response.redirect(contextURL)
-            except:
-                self.status = "Noe gikk gale med reservasjonen"
+                except:
+                    self.status = "Noe gikk gale med reservasjonen"
         else: 
             self.status = "Noe gikk gale med reservasjonen"
     
